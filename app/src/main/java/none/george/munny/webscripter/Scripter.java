fa503b9717018b9c99f1303248ed29116fe06f7c
George Varghese M <georgevarghese185@yahoo.com>
2018-09-08 00:36:04 +0530

    Update Scripter class
    - Accumulate results at each step
    - Catch WebView errors

--- app/src/main/java/none/george/munny/webscripter/Scripter.java
+++ app/src/main/java/none/george/munny/webscripter/Scripter.java
@@ -1,14 +1,25 @@
 package none.george.munny.webscripter;
 
+import android.net.http.SslError;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scripter {
     public static final String INTERFACE_NAME = "StepInterface";
@@ -17,13 +28,15 @@ public class Scripter {
     private WebView webView;
     private CompleteListener completeListener;
     private int currentStep;
+    private List<String> resultAccumulator;
 
     public interface CompleteListener {
-        void onComplete(@Nullable String result);
+        void onComplete(List<String> resultAccumulator);
     }
 
     private Scripter(List<Step> steps) {
         this.steps = steps;
+        resultAccumulator = new ArrayList<>();
     }
 
 
@@ -36,20 +49,28 @@ public class Scripter {
         this.webView.setWebViewClient(client);
         this.webView.addJavascriptInterface(_interface, INTERFACE_NAME);
 
-        executeStep(0, null);
+        executeStep(0);
     }
 
-    private void executeStep(int stepNumber, @Nullable String result) {
+    private void executeStep(int stepNumber) {
         if(stepNumber < steps.size()) {
             currentStep = stepNumber;
             steps.get(stepNumber).execute(webView);
         } else {
-            runOnUiThread(() -> {
-                webView.removeJavascriptInterface(INTERFACE_NAME);
-                webView.setWebViewClient(null);
-                completeListener.onComplete(result);
-            });
+            end();
         }
+    }
+
+    private void end() {
+        runOnUiThread(() -> {
+            webView.removeJavascriptInterface(INTERFACE_NAME);
+            webView.setWebViewClient(null);
+            completeListener.onComplete(resultAccumulator);
+        });
+    }
+
+    private void accumulate(@Nullable String result) {
+        resultAccumulator.add(result);
     }
 
 
@@ -79,20 +100,38 @@ public class Scripter {
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            executeStep(currentStep + 1, null);
+            accumulate(url);
+            executeStep(currentStep + 1);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+            accumulate(error.toString());
+            end();
+        }
+
+        @Override
+        public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+            super.onReceivedHttpError(view, request, errorResponse);
+            accumulate(writeErrorResponse(errorResponse).toString());
+            end();
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            super.onReceivedSslError(view, handler, error);
+            accumulate( error.getPrimaryError() + ":" + error.getUrl());
+            end();
         }
     }
 
 
     private class Interface {
         @JavascriptInterface
-        public void onCommandDone() {
-            executeStep(currentStep + 1, null);
-        }
-
-        @JavascriptInterface
-        public void onCommandDone(String result) {
-            executeStep(currentStep + 1, result);
+        public void onCommandDone(@Nullable String result) {
+            accumulate(result);
+            executeStep(currentStep + 1);
         }
     }
 
@@ -130,6 +169,50 @@ public class Scripter {
 
         public Scripter build() {
             return new Scripter(this.steps);
+        }
+    }
+
+
+
+    private static JSONObject writeErrorResponse(WebResourceResponse resourceError) {
+        JSONObject error = new JSONObject();
+        BufferedReader reader = null;
+        try {
+            error.put("encoding", resourceError.getEncoding());
+            error.put("mime-type", resourceError.getMimeType());
+            error.put("reason", resourceError.getReasonPhrase());
+            error.put("code", resourceError.getStatusCode());
+
+            JSONObject headers = new JSONObject();
+            for(Map.Entry<String, String> entry : resourceError.getResponseHeaders().entrySet()) {
+                headers.put(entry.getKey(), entry.getValue());
+            }
+
+            error.put("headers", headers);
+
+            reader = new BufferedReader(new InputStreamReader(resourceError.getData()));
+            StringBuilder errorData = new StringBuilder();
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                errorData.append(line);
+            }
+
+            error.put("data", errorData.toString());
+
+            return error;
+        } catch (Exception e) {
+            HashMap<String, String> hashMap = new HashMap<>();
+            hashMap.put("ERROR_RESPONSE_EXCEPTION", e.toString());
+            hashMap.put("INCOMPLETE_ERROR_RESPONSE", error.toString());
+
+            return error;
+        } finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {/*Ignored*/}
+            }
         }
     }
 
