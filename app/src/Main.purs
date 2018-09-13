b3b255c9298d37eec974dd17ffb0f67ca63759f3
George Varghese M <georgevarghese185@yahoo.com>
2018-09-13 23:09:34 +0530

    Add test app that opens techradar.com and fetches heaedlines

--- app/src/Main.purs
+++ app/src/Main.purs
@@ -2,15 +2,38 @@ module Main where
 
 import Prelude
 
+import Data.Either (Either(..))
 import Effect (Effect)
-import Effect.Console (log)
-import Interface (setupInterface)
+import Effect.Aff (Aff, Error, runAff_)
+import Effect.Class.Console (log, logShow)
+import Interface (exit, setupInterface)
 import Interface.Events (setupEvents)
+import WebScripter (Script(..), ScriptStep(..), ScripterId(..), createScripter, executeScripter)
 
 main :: Effect Unit
 main = do
-  log "Setting up Interface"
-  setupInterface
-  log "Setting LifeCycle listener"
-  setupEvents
+  log "Setting up Interface" *> setupInterface
+  log "Setting up Event listeners" *> setupEvents
   log "Startup complete"
+  runAff_ handler app
+  where
+    handler :: Either Error Unit -> Effect Unit
+    handler _ = exit
+
+app :: Aff Unit
+app = do
+  let
+    id = ScripterId "test"
+    script = Script [
+      URL "https://techradar.com",
+      JS $    "var articleNames = Array.from(document.getElementsByClassName(\"article-name\")).map(h => h.innerHTML);"
+					<>  "Interface.onCommandDone(JSON.stringify(articleNames));"
+    ]
+  log "Creating scripter"
+  createScripter id
+  log "executing"
+  result <- executeScripter id script
+  log "back from scripter"
+  case result of
+    Right results -> logShow results
+    Left e -> logShow e
