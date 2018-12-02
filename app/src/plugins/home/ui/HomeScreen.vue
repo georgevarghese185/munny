e28c000d837d2920d152d823eaef89f45b248a3d
George Varghese M <georgevarghese185@yahoo.com>
2018-12-02 19:36:21 +0530

    Add HomeScreen UI with NavBar

---
+++ app/src/plugins/home/ui/HomeScreen.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <div class="home-screen">
+    <NavBar :app="app" :title="app.name"/>
+  </div>
+
+</template>
+
+
+
+
+<script>
+  import NavBar from './widgets/NavBar.vue'
+
+  export default {
+    data: function(){
+      return {
+        app: {
+          name: "Munny",
+          pluginDir: "home"
+        }
+      }
+    },
+    components: {
+      NavBar
+    }
+  }
+
+</script>
+
+
+
+
+<style scoped>
+
+  .home-screen {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #fbfbfb;
+  }
+
+</style>
---
+++ app/src/plugins/home/ui/widgets/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+
+  <div class="nav-bar">
+    <div class="nav-button">
+      <img class="nav-bar-icon" :src="`${app.pluginDir}/assets/nav.png`"/>
+    </div>
+    <p class="title"> {{title}} </p>
+  </div>
+
+</template>
+
+
+
+
+<script>
+
+  export default {
+    props: ["app", "title"]
+  }
+
+</script>
+
+
+
+
+<style scoped>
+
+  p {
+    font-family: sans-serif;
+  }
+
+  .nav-bar {
+    position: relative;
+    background-color: #333333;
+    border-bottom: #cecece solid 2px;
+  }
+
+  .title {
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin: 0;
+    display: inline-block;
+    width: 100%;
+    height: 48px;
+    line-height: 48px;
+    color: #f5f5f5;
+    font-size: 22px;
+    text-align: center;
+  }
+
+  .nav-button {
+    width: 56px;
+    height: 48px;
+  }
+
+  .nav-bar-icon {
+    position: absolute;
+    display: inline-block;
+    width: 35px;
+    height: 28px;
+    margin: 10px 10px;
+  }
+
+</style>
