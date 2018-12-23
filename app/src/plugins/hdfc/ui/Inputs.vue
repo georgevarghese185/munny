a6fca4d53c24f7cf85e53e93a6306522c862f8f4
George Varghese M <georgevarghese185@yahoo.com>
2018-12-23 22:53:04 +0530

    Add HDFC inputs dialog

---
+++ app/src/plugins/hdfc/ui/Inputs.vue
@@ -0,0 +1,65 @@
+<template>
+
+	<div class="dialog-contents">
+    <p class="sensitive light-text"> *Sensitive </p>
+    <input class="field" type="email" placeholder="Customer ID"/>
+    <p class="sensitive light-text"> *Sensitive </p>
+    <input class="field" type="password" placeholder="Password"/>
+    <p class="sensitive-note light-text"> *Sensitive fields will be encrypted and stored on your device </p>
+    <Button label="OK"/>
+  </div>
+
+</template>
+
+
+
+
+<script>
+  import Button from 'src/plugins/home/ui/widgets/Button.vue';
+
+	export default {
+    components: { Button }
+  }
+
+</script>
+
+
+
+
+<style scoped>
+
+  p {
+    font-family: sans-serif;
+  }
+
+  .dialog-contents {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .field {
+    background-color: #ffffff;
+    border: 1px solid #000000;
+    padding: 0 8px;
+    height: 42px;
+    width: 90%;
+  }
+
+  .sensitive {
+    width: 90%;
+    margin: 14px 0 4px 0;
+    padding-left: 20px;
+  }
+
+  .sensitive-note {
+    margin-top: 32px;
+    width: 90%;
+  }
+
+  .light-text {
+    font-size: 11px;
+    color: #8e8e8e;
+  }
+
+</style>
