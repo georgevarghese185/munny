d0495d6d70924ac98e02a82a1d84c636f65851a5
George Varghese M <georgevarghese185@yahoo.com>
2019-01-14 22:09:14 +0530

    Remove -'s in inputs/outputs, restructure plugin inputs format

--- app/src/plugins/hdfc/Plugin/HDFC/Main.purs
+++ app/src/plugins/hdfc/Plugin/HDFC/Main.purs
@@ -8,21 +8,26 @@ import App.Plugin (pluginReady)
 import App.Plugin.UI (wait)
 import Control.Alt ((<|>))
 import Control.Monad.Error.Class (throwError)
-import Control.Monad.Except (runExcept)
-import Data.Either (Either(..), hush)
+import Data.Either (Either(..))
 import Data.Foldable (elem)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Exception (error)
-import Foreign (F, Foreign)
-import Foreign.Class (decode)
-import Foreign.Index (index)
+import Foreign (Foreign)
 import Plugin.HDFC (pluginName)
 import Plugin.HDFC.Settings (HdfcSettings)
 import Plugin.HDFC.UI.Settings (SettingsScreenEvent(..), startSettingsScreen)
 import Simple.JSON (read, write)
+
+type Params = {
+  inputs :: {
+    ui :: String,
+    serviceAccountSettings :: Maybe HdfcSettings
+  },
+  outputs :: Array String
+}
 
 main :: Effect Unit
 main = pluginReady pluginName start
@@ -35,15 +40,15 @@ settingsDone :: SettingsScreenEvent -> Maybe HdfcSettings
 settingsDone (SettingsDone customerId password) = Just {customerId, password}
 
 settingsRequest :: Foreign -> Aff Foreign
-settingsRequest inputs = do
-  rootId <- case read inputs of
+settingsRequest fgn = do
+  (p :: Params) <- case read fgn of
     Right inputs' -> pure inputs'
     Left e -> throwError $ error $ show e
-  case elem "service-accounts-settings" <$> runExcept ((index inputs "outputs" >>= decode) :: F (Array String)) of
-    Right true -> pure unit
-    _ -> throwError $ error "Unknown request"
-  let currentSettings = hush $ (runExcept $ index inputs "service-account-settings") >>= read
-  ui <- liftEffect $ startSettingsScreen rootId currentSettings
+  if elem "service-accounts-settings" p.outputs
+    then pure unit
+    else throwError $ error "Unknown request"
+  let currentSettings = p.inputs.serviceAccountSettings
+  ui <- liftEffect $ startSettingsScreen p.inputs.ui currentSettings
   settings <- wait ui settingsDone
   pure $ write settings
 
